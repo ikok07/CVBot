f2e78c9c072a49cf957812ea6a6e0c2dffb39f83
Kaloyan Stefanov <kokmarok@gmail.com>
2025-09-11 22:14:47 +0300

    remove logs from protect middleware

--- src/routes/dependencies/protect.py
+++ src/routes/dependencies/protect.py
@@ -11,30 +11,32 @@ from src.models.db.profiles import Profile
 from src.models.errors.api import APIError
 
 async def protect_dependency(request: Request) -> (User, Profile):
-    sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
-    print("Initializing Clerk...")
-    def auth():
-        state = sdk.authenticate_request(
-            request=request,
-            options=AuthenticateRequestOptions()
-        )
-        print("Clerk authentication finished")
-        print(state)
-        if state.status == AuthStatus.SIGNED_IN:
-            user = sdk.users.get(user_id=state.payload["sub"])
-            print("Clerk user checked")
-            print(user)
-            if not user:
-                raise APIError(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, message="User not found")
-            return user
-        else:
-            raise APIError(status_code=status.HTTP_401_UNAUTHORIZED, message="Unauthorized")
-
-    clerk_user = await asyncio.to_thread(auth)
-    print("Fetching profile from db...")
     try:
+        sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
+
+        def auth():
+            state = sdk.authenticate_request(
+                request=request,
+                options=AuthenticateRequestOptions()
+            )
+
+            if state.status == AuthStatus.SIGNED_IN:
+                user = sdk.users.get(user_id=state.payload["sub"])
+                if not user:
+                    raise APIError(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, message="User not found")
+                return user
+            else:
+                raise APIError(status_code=status.HTTP_401_UNAUTHORIZED, message="Unauthorized")
+
+        clerk_user = await asyncio.to_thread(auth)
         profile = await Profile.get(id=clerk_user.id)
         return clerk_user, profile
     except DoesNotExist as e:
         print("User profile does not exist!")
-    raise APIError(status_code=status.HTTP_401_UNAUTHORIZED, message="Unauthorized")
+        raise APIError(status_code=status.HTTP_401_UNAUTHORIZED, message="Unauthorized")
+    except APIError as e:
+        raise e
+    except Exception as e:
+        print("Generic error in protect middleware:")
+        print(e)
+        raise APIError(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, message="Something went wrong during authentication")
