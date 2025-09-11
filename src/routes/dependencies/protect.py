2fe051b4dbfc21d6b7a7beffe1c34e3588f4b5be
Kaloyan Stefanov <kokmarok@gmail.com>
2025-09-11 22:04:54 +0300

    add logs to protect middleware

--- src/routes/dependencies/protect.py
+++ src/routes/dependencies/protect.py
@@ -13,15 +13,17 @@ from src.models.errors.api import APIError
 async def protect_dependency(request: Request) -> (User, Profile):
     try:
         sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
-
+        print("Initializing Clerk...")
         def auth():
             state = sdk.authenticate_request(
                 request=request,
                 options=AuthenticateRequestOptions()
             )
-
+            print("Clerk authentication finished")
             if state.status == AuthStatus.SIGNED_IN:
                 user = sdk.users.get(user_id=state.payload["sub"])
+                print("Clerk user checked")
+                print(user)
                 if not user:
                     raise APIError(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, message="User not found")
                 return user
@@ -29,11 +31,13 @@ async def protect_dependency(request: Request) -> (User, Profile):
                 raise APIError(status_code=status.HTTP_401_UNAUTHORIZED, message="Unauthorized")
 
         clerk_user = await asyncio.to_thread(auth)
+        print("Fetching profile from db...")
         profile = await Profile.get(id=clerk_user.id)
         return clerk_user, profile
     except DoesNotExist as e:
         print("User profile does not exist!")
         raise APIError(status_code=status.HTTP_401_UNAUTHORIZED, message="Unauthorized")
     except Exception as e:
+        print("Generic error in protect middleware:")
         print(e)
         raise APIError(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, message="Something went wrong during authentication")
